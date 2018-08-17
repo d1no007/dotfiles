5ffa2ca6d30c747ff865aed577a3d85d84418795
dino rodriguez <dinorodriguez@college.harvard.edu>
2018-08-17 15:12:08 -0700

    feat: useful scripts
    First script is a startup script for updating shell packages.

---
+++ .scripts/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# source scripts
+. ~/.zshrc
+
+# colors
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# brew
+echo "${green}updating all homebrew packages..${reset}"
+brew update
+brew upgrade
+brew cleanup
+brew prune 
+brew doctor
+
+# node 
+echo "${green}updating node and npm along with packages..${reset}"
+nvm install-latest-npm
